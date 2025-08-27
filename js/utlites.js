3da5eea1a444fc43b205ac6fe0627fb992a5edf8
Sazzadkhan45 <sazzadkhan753@gmail.com>
2025-08-27 11:39:32 +0600

    Js function start now

---
+++ js/utlites.js
@@ -0,0 +1,8 @@
+// Coin balance function
+function coinNumberCovertedNumber(id){
+    const coinNumberStr = document.getElementById(id).innerText;
+    const coinNumber = parseInt(coinNumberStr);
+    return coinNumber;
+}
+
+// ----
